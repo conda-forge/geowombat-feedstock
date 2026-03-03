787fcfdee1187554eb83de68acb798d0997997a1
mmann1123 <mmann1123@gmail.com>
2026-03-03 16:34:33 -0500

    cross compilation

--- recipe/install-geowombat.sh
+++ recipe/install-geowombat.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -ex
 
-${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
+# For cross-compilation, pass meson args (including --cross-file) through pip
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+  ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation \
+    -Csetup-args=${MESON_ARGS// / -Csetup-args=}
+else
+  ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
+fi
